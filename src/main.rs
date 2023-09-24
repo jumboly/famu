b26f7c2f721709e8a73dfd32a5b0db276144399d
Masanori Kitani <jumboly@gmail.com>
2023-09-24 10:51:44 +0900

    3.2 Memory addressing mode
    * リセット実装

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ pub struct CPU {
     pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
+    memory: [u8; 0xFFFF],
 }
 
 impl CPU {
@@ -16,6 +17,59 @@ impl CPU {
             register_x: 0,
             status: 0,
             program_counter: 0,
+            memory: [0u8; 0xFFFF],
+        }
+    }
+
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+        self.program_counter = self.mem_read_u16(0xFFFC);
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.mem_write_u16(0xFFFC, 0x8000);
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run();
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            let opscode = self.mem_read(self.program_counter);
+            self.program_counter += 1;
+
+            match opscode {
+                0xA9 => {
+                    let param = self.mem_read(self.program_counter);
+                    self.program_counter += 1;
+
+                    self.lda(param);
+                },
+                0xAA => {
+                    self.tax();
+                },
+                0xE8 => {
+                    self.inx();
+                }
+                0x00 => {
+                    return;
+                }
+                _ => todo!("")
+            }
         }
     }
 
@@ -32,6 +86,18 @@ impl CPU {
     fn tax(&mut self) {
         self.register_x = self.register_a;
         self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn mem_read_u16(&self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos);
+        let hi = self.mem_read(pos + 1);
+        u16::from_le_bytes([lo, hi])
+    }
+
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let [lo, hi] = u16::to_le_bytes(data);
+        self.mem_write(pos, lo);
+        self.mem_write(pos + 1, hi);
     }
 
     fn update_zero_and_negative_flags(&mut self, result: u8) {
@@ -51,31 +117,9 @@ impl CPU {
     }
 
     pub fn interpret(&mut self, program: Vec<u8>) {
-        self.program_counter = 0;
-
-        loop {
-            let opscode = program[self.program_counter as usize];
-            self.program_counter += 1;
-
-            match opscode {
-                0xA9 => {
-                    let param = program[self.program_counter as usize];
-                    self.program_counter += 1;
-
-                    self.lda(param);
-                },
-                0xAA => {
-                    self.tax();
-                },
-                0xE8 => {
-                    self.inx();
-                }
-                0x00 => {
-                    return;
-                }
-                _ => todo!("")
-            }
-        }
+        self.load(program);
+        self.program_counter = self.mem_read_u16(0xFFFC);
+        self.run();
     }
 }
 
